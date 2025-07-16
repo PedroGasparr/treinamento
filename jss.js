0611f92cef2f6b2d2b134375e525aedf9f23a87d
Pedro Henrique Gaspar <106278541+PedroGasparr@users.noreply.github.com>
2025-07-16 16:24:48 -0300

    1.0

---
+++ jss.js
@@ -0,0 +1,16 @@
+let descricao = ["Descrição 1", "Descrição 2", "Descrição 3"];
+
+function descricao1 () {
+    let descricao1 = document.getElementById("descricao1");
+    descricao1.innerHTML = descricao[0];
+}
+
+function descricao2 () {
+    let descricao2 = document.getElementById("descricao2");
+    descricao2.innerHTML = descricao[1];
+}
+
+function descricao3 () {
+    let descricao3 = document.getElementById("descricao3");
+    descricao3.innerHTML = descricao[2];
+}
